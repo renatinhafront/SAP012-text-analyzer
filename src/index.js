add2cc80e562268011c2c6ef4fdee4888b39dcbd
renatinhafront <re.saraiva09@hotmail.com>
2023-12-19 15:36:31 -0300

    index.js ok

--- src/index.js
+++ src/index.js
@@ -11,13 +11,22 @@ let wordCountText = wordCount.textContent
 let characterCount = document.querySelector('[data-testid="character-count"]');
 let characterCountText = characterCount.textContent
 
+//Contador excluindo caracteres
+let characterExcluido = document.querySelector('[data-testid="character-no-spaces-count"]');
+let characterExcluidoText = characterExcluido.textContent
+
 // Contador de numeros
 let numberCount = document.querySelector('[data-testid="number-count"]');
 let numberCountText = numberCount.textContent
 
+//Soma total de numeros
+let somaCount = document.querySelector('[data-testid="number-sum"]');
+let somaCountText = somaCount.textContent
 
 
-
+//Comprimento medio das palavras
+let comprimentoCount = document.querySelector('[data-testid="word-length-average"]');
+let comprimentoCountText = comprimentoCount.textContent
 
 //Retorna o primeiro elemento dentro do documento 
 let textArea = document.querySelector('textarea[name="user-input"]');
@@ -26,6 +35,10 @@ textArea.addEventListener('keyup', function() {
    wordCount.textContent = wordCountText + analyzer.getWordCount(textArea.value);
    characterCount.textContent = characterCountText + analyzer.getCharacterCount(textArea.value);
    numberCount.textContent = numberCountText + analyzer.getNumberCount(textArea.value);
+   characterExcluido.textContent = characterExcluidoText + analyzer.getCharacterCountExcludingSpaces(textArea.value);
+   somaCount.textContent = somaCountText + analyzer.getNumberSum(textArea.value);
+   comprimentoCount.textContent = comprimentoCountText + analyzer.getAverageWordLength(textArea.value);
+
 });   
 
 //Retorna a referência do elemento através do seu ID.
@@ -42,8 +55,3 @@ function countWord() {
         .getElementById("user-input").value;
     
 }
-
-
-
-
-
