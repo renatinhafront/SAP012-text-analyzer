b946e3f2e9861560b6e5e0fcbb00bf215dd8a2e9
renatinhafront <re.saraiva09@hotmail.com>
2023-12-19 22:18:47 -0300

    Falta if..else e o reset nas li

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,35 +1,64 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-   
+
     // Criei uma variavel do tipo let e removi os espaços do final do texto usando a funçao trim
-    let wordTrim = text.trim();
+    const wordTrim = text.trim();
     // Criei uma variavel do tipo let e usei o split que quebra o texto com o separador e cria uma lista de palavras
-    let split = wordTrim.split(' ');
-    // Retornei o tamanho da lista usando length
+    const split = wordTrim.split(' ');
+    // Retornei o tamanho da lista usando length(tamanho da lista)
     return split.length
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
     // Criei uma variavel do tipo let e atribui o valor do tamanho do texto atraves do length e converti (parseInt) o valor de string para inteiro
-    let countText = parseInt(text.length)
+    const countText = parseInt(text.length)
     return countText
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+    //no regex usei o ^ como negação tudo que não for caracteres especiais e espaços ele remove
+    const characterExcluido = text.replace(/[^0-9a-zA-Z]/gim, '');
+    return characterExcluido.length
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    // Criei uma variavel do tipo let e removi os espaços do final do texto usando a funçao trim
+    const wordTrim = text.trim();
+    // Criei uma variavel do tipo let e usei o split que quebra o texto com o separador e cria uma lista de palavras
+    const split = wordTrim.split(' ');
+    // criei u
+    let media = 0;
+
+    for (let i = 0; i < split.length; i++) {
+      media = media + split[i].length;
+    }
+
+    const resultado = media / split.length;
+    return resultado;
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+
     // replace substitui o caracter de acordo com o regex informado.  
-    let numberCount = text.replace(/\D/gim, '');
+    const numberCount = text.replace(/\D|./gim, '');
     return numberCount.length
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numberCount = text.replace(/\D|./gim, '');
+    //
+    const split = numberCount.split('');
+    let total = 0;
+    for (let i = 0; i < split.length; i++) {
+      const valor = split[i];
+      total = total + parseFloat(valor);
+    }
+    return total;
   },
 };
 
