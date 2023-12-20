24edcac633295236f075462639b740e6744b62f1
renatinhafront <re.saraiva09@hotmail.com>
2023-12-19 22:16:57 -0300

    teste uni ok

--- src/index.js
+++ src/index.js
@@ -4,54 +4,45 @@ import analyzer from './analyzer.js';
 //querySelector = Busca o elemento no HTML atraves do atributo data-testid
 
 // Contador de palavras
-let wordCount = document.querySelector('[data-testid="word-count"]');
-let wordCountText = wordCount.textContent
+const wordCount = document.querySelector('[data-testid="word-count"]');
+const wordCountText = wordCount.textContent
 
 // Contador de caracteres
-let characterCount = document.querySelector('[data-testid="character-count"]');
-let characterCountText = characterCount.textContent
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterCountText = characterCount.textContent
 
 //Contador excluindo caracteres
-let characterExcluido = document.querySelector('[data-testid="character-no-spaces-count"]');
-let characterExcluidoText = characterExcluido.textContent
+const characterExcluido = document.querySelector('[data-testid="character-no-spaces-count"]');
+const characterExcluidoText = characterExcluido.textContent
 
 // Contador de numeros
-let numberCount = document.querySelector('[data-testid="number-count"]');
-let numberCountText = numberCount.textContent
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberCountText = numberCount.textContent
 
 //Soma total de numeros
-let somaCount = document.querySelector('[data-testid="number-sum"]');
-let somaCountText = somaCount.textContent
+const somaCount = document.querySelector('[data-testid="number-sum"]');
+const somaCountText = somaCount.textContent
 
 
-//Comprimento medio das palavras
-let comprimentoCount = document.querySelector('[data-testid="word-length-average"]');
-let comprimentoCountText = comprimentoCount.textContent
+//Media das palavras
+const mediaCount = document.querySelector('[data-testid="word-length-average"]');
+const mediaCountText = mediaCount.textContent
 
 //Retorna o primeiro elemento dentro do documento 
-let textArea = document.querySelector('textarea[name="user-input"]');
-textArea.addEventListener('input', function() {
-   
-   wordCount.textContent = wordCountText + analyzer.getWordCount(textArea.value);
-   characterCount.textContent = characterCountText + analyzer.getCharacterCount(textArea.value);
-   numberCount.textContent = numberCountText + analyzer.getNumberCount(textArea.value);
-   characterExcluido.textContent = characterExcluidoText + analyzer.getCharacterCountExcludingSpaces(textArea.value);
-   somaCount.textContent = somaCountText + analyzer.getNumberSum(textArea.value);
-   comprimentoCount.textContent = comprimentoCountText + analyzer.getAverageWordLength(textArea.value);
+const textArea = document.querySelector('textarea[name="user-input"]');
+textArea.addEventListener('input', function () {
 
-});   
+  wordCount.textContent = wordCountText + analyzer.getWordCount(textArea.value);
+  characterCount.textContent = characterCountText + analyzer.getCharacterCount(textArea.value);
+  numberCount.textContent = numberCountText + analyzer.getNumberCount(textArea.value);
+  characterExcluido.textContent = characterExcluidoText + analyzer.getCharacterCountExcludingSpaces(textArea.value);
+  somaCount.textContent = somaCountText + analyzer.getNumberSum(textArea.value);
+  mediaCount.textContent = mediaCountText + analyzer.getAverageWordLength(textArea.value);
+
+});
 
 //Retorna a referência do elemento através do seu ID.
-let button = document.getElementById("reset-button");
-button.addEventListener('click', function() {
-    textArea.value = "";
-}); 
-
-
-function countWord() {
-  
-    // Get the input text value
-    let words = document
-        .getElementById("user-input").value;
-    
-}
+const button = document.getElementById("reset-button");
+button.addEventListener('click', function () {
+  textArea.value = "";
+});
