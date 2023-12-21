2a75709e14962edc078aca042a07bc14fd5f57c9
renatinhafront <re.saraiva09@hotmail.com>
2023-12-21 18:59:15 -0300

    review

--- src/analyzer.js
+++ src/analyzer.js
@@ -2,96 +2,57 @@ const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
-    // Criei uma variavel do tipo let e removi os espaços do final do texto usando a funçao trim
-    const wordTrim = text.trim();
-    // Criei uma variavel do tipo let e usei o split(lista) que quebra o texto com o separador e cria uma lista de palavras
-    const lista = wordTrim.split(' ');
-
-    //Criei uma lista vazia para armazenar as palavras maiores que um caracter
-    const wordList = []
-    //for é o looping / para capturar somente palavras do texto
-    for (let i = 0; i < lista.length; i++) {
-      //if SE o valor aramazenado for maior que um é considerado uma palavra
-      if (lista[i].length > 1) {
-        //if para verificar se há alguma palavra armazenada na lista no indice 0
-        if (wordList.length === 0) {
-          //adicionando a palavra no indice 0 da lista
-          wordList[0] = lista[i]
-          //else caso contrario
-        } else {
-          //adiciono no ultimo bloco disponivel da lista a palavra
-          wordList[wordList.length] = lista[i]
-        }
-      }
-    }
-    // Retornei o tamanho da lista usando length(tamanho da lista)
-    return wordList.length
+    const words = text.trim().split(/\s+/);
+    const filteredWords = words.filter(word => word.length > 1);
+    return filteredWords.length;
   },
 
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
 
-    // Criei uma variavel do tipo let e atribui o valor do tamanho do texto atraves do length e converti (parseInt) o valor de string para inteiro
-    const countText = parseInt(text.length)
-    //retorna a quantidade de caracteres
-    return countText
+    return text.length;
   },
 
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
 
-    //no regex usei o ^ como negação tudo que não for caracteres especiais e espaços ele remove
     const characterExcluido = text.replace(/[^0-9a-zA-Z]/gim, '');
-    //retorna a quantidade de caracteres excluindo espacos e pontos na lista
     return characterExcluido.length
   },
 
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
-    // Criei uma variavel do tipo let e removi os espaços do final do texto usando a funçao trim
-    const wordTrim = text.trim();
-    // Criei uma variavel do tipo let e usei o split que quebra o texto com o separador e cria uma lista de palavras
-    const split = wordTrim.split(' ');
-    // criei uma variavel com o valor de 0, que vai armazenar a média
-    let totalCaracteres = 0;
-    //FOR para somar a quantidade de caracteres digitados, i=indice da lista, splite.length=retorna o tmanho da lista
-    for (let i = 0; i < split.length; i++) {
-      //totalcaracteres é do tipo inteiro(numero) + o tamanho da lista
-      totalCaracteres = totalCaracteres + split[i].length;
-    }
-    //Criei uma constante media = numero da lista dividido pelo tamanho da lista
-    const media = totalCaracteres / split.length;
-    //converte em decimal com dois digitos e retorna
-    return parseFloat(media.toFixed(2));
+    const words = text.trim().split(/\s+/);
+    const totalCharacters = words.reduce((acc, word) => acc + word.length, 0);
+    const average = totalCharacters / words.length || 0;
+    return parseFloat(average.toFixed(2));
   },
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
 
-    // replace substitui o caracter de acordo com o regex informado(extrai somente numeros e pontos).
-    const numberCount = text.replace(/\D/gim, '');
-    //retorna a quantidade de numeros digitados
-    return numberCount.length
+    const numberCount = text.match(/\b\d+(\.\d+)?\b/g);
+    return numberCount ? numberCount.length : 0;
   },
 
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const numberCount = text.replace(/\D/gim, '');
-    //
-    const split = numberCount.split('');
-    //criei  uma variavel total com o valor de 0 que vai armazenar o valor total da lista
-    let total = 0;
-    //FOR para somar a quantidade de caracteres digitados, i=indice da lista, splite.length=retorna o tmanho da lista
-    for (let i = 0; i < split.length; i++) {
-      //criei uma constante para armazenar o tamanho da palavra
-      const valor = split[i];
-      //parseFloat ele converte string(conjunto de caracteres) para numeros decimais.
-      total = total + parseFloat(valor);
+    const numerosEncontrados = text.match(/\b\d+(\.\d+)?\b/g);
+
+    if (numerosEncontrados) {
+      let total = 0;
+
+      for (let i = 0; i < numerosEncontrados.length; i++) {
+        total += Number(numerosEncontrados[i]);
+      }
+
+      return total;
+    } else {
+      return 0;
     }
-    //retorno total da tela
-    return total;
-  },
-};
+  }
+}
+
 
 export default analyzer;
