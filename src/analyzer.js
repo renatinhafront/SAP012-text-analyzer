f3d75abd953bfb443ec4091cb836c685feabc976
renatinhafront <re.saraiva09@hotmail.com>
2023-12-20 21:30:06 -0300

    Inclusão do reset na li e ajuste no contador de palavras

--- src/analyzer.js
+++ src/analyzer.js
@@ -4,10 +4,28 @@ const analyzer = {
 
     // Criei uma variavel do tipo let e removi os espaços do final do texto usando a funçao trim
     const wordTrim = text.trim();
-    // Criei uma variavel do tipo let e usei o split que quebra o texto com o separador e cria uma lista de palavras
-    const split = wordTrim.split(' ');
+    // Criei uma variavel do tipo let e usei o split(lista) que quebra o texto com o separador e cria uma lista de palavras
+    const lista = wordTrim.split(' ');
+
+    //Criei uma lista vazia para armazenar as palavras maiores que um caracter
+    const wordList = []
+    //for é o looping / para capturar somente palavras do texto
+    for (let i = 0; i < lista.length; i++) {
+      //if SE o valor aramazenado for maior que um é considerado uma palavra
+      if (lista[i].length > 1) {
+        //if para verificar se há alguma palavra armazenada na lista no indice 0
+        if (wordList.length === 0) {
+          //adicionando a palavra no indice 0 da lista
+          wordList[0] = lista[i]
+          //else caso contrario
+        } else {
+          //adiciono no ultimo bloco disponivel da lista a palavra
+          wordList[wordList.length] = lista[i]
+        }
+      }
+    }
     // Retornei o tamanho da lista usando length(tamanho da lista)
-    return split.length
+    return wordList.length
   },
 
   getCharacterCount: (text) => {
@@ -40,18 +58,18 @@ const analyzer = {
     //FOR para somar a quantidade de caracteres digitados, i=indice da lista, splite.length=retorna o tmanho da lista
     for (let i = 0; i < split.length; i++) {
       //totalcaracteres é do tipo inteiro(numero) + o tamanho da lista
-     totalCaracteres = totalCaracteres + split[i].length;
+      totalCaracteres = totalCaracteres + split[i].length;
     }
     //Criei uma constante media = numero da lista dividido pelo tamanho da lista
     const media = totalCaracteres / split.length;
     //retorna a media na tela
-    return media.toFixed(0);
+    return media.toFixed(2);
   },
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
 
-    // replace substitui o caracter de acordo com o regex informado(extrai somente numeros e pontos).  
+    // replace substitui o caracter de acordo com o regex informado(extrai somente numeros e pontos).
     const numberCount = text.replace(/\D/gim, '');
     //retorna a quantidade de numeros digitados
     return numberCount.length
@@ -66,10 +84,10 @@ const analyzer = {
     let total = 0;
     //FOR para somar a quantidade de caracteres digitados, i=indice da lista, splite.length=retorna o tmanho da lista
     for (let i = 0; i < split.length; i++) {
-    //criei uma constante para armazenar o tamanho da palavra
-    const valor = split[i];
-    //parseFloat ele converte string(conjunto de caracteres) para numeros decimais.
-    total = total + parseFloat(valor);
+      //criei uma constante para armazenar o tamanho da palavra
+      const valor = split[i];
+      //parseFloat ele converte string(conjunto de caracteres) para numeros decimais.
+      total = total + parseFloat(valor);
     }
     //retorno total da tela
     return total;
--- src/index.js
+++ src/index.js
@@ -5,44 +5,63 @@ import analyzer from './analyzer.js';
 
 // Contador de palavras
 const wordCount = document.querySelector('[data-testid="word-count"]');
-const wordCountText = wordCount.textContent
+const wordCountText = "Contagem de palavras: "
 
 // Contador de caracteres
 const characterCount = document.querySelector('[data-testid="character-count"]');
-const characterCountText = characterCount.textContent
+const characterCountText = "Contagem de caracteres: "
 
 //Contador excluindo caracteres
-const characterExcluido = document.querySelector('[data-testid="character-no-spaces-count"]');
-const characterExcluidoText = characterExcluido.textContent
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const characterNoSpacesCountText = "Contagem sem espaços e pontuação: "
 
 // Contador de numeros
 const numberCount = document.querySelector('[data-testid="number-count"]');
-const numberCountText = numberCount.textContent
+const numberCountText = "Contagem de números: "
 
 //Soma total de numeros
-const somaCount = document.querySelector('[data-testid="number-sum"]');
-const somaCountText = somaCount.textContent
-
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const numberSumText = "Soma total de números: "
 
 //Media das palavras
-const mediaCount = document.querySelector('[data-testid="word-length-average"]');
-const mediaCountText = mediaCount.textContent
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
+const wordLengthAverageText = "Comprimento médio das palavras: "
 
-//Retorna o primeiro elemento dentro do documento 
+//Retorna o primeiro elemento dentro do documento
 const textArea = document.querySelector('textarea[name="user-input"]');
 textArea.addEventListener('input', function () {
-
   wordCount.textContent = wordCountText + analyzer.getWordCount(textArea.value);
   characterCount.textContent = characterCountText + analyzer.getCharacterCount(textArea.value);
   numberCount.textContent = numberCountText + analyzer.getNumberCount(textArea.value);
-  characterExcluido.textContent = characterExcluidoText + analyzer.getCharacterCountExcludingSpaces(textArea.value);
-  somaCount.textContent = somaCountText + analyzer.getNumberSum(textArea.value);
-  mediaCount.textContent = mediaCountText + analyzer.getAverageWordLength(textArea.value);
+  characterNoSpacesCount.textContent = characterNoSpacesCountText + analyzer.getCharacterCountExcludingSpaces(textArea.value);
+  numberSum.textContent = numberSumText + analyzer.getNumberSum(textArea.value);
+  wordLengthAverage.textContent = wordLengthAverageText + analyzer.getAverageWordLength(textArea.value);
 
 });
 
 //Retorna a referência do elemento através do seu ID.
 const button = document.getElementById("reset-button");
 button.addEventListener('click', function () {
-  textArea.value = "";
+  initialize()
 });
+
+//executa a inicializacao
+initialize();
+
+//funcao que concatena o texto e o contador
+function concat(text, count) {
+  return text + count
+}
+
+//inicializa com 0 as caixas
+function initialize() {
+  textArea.value = "";
+  wordCount.textContent = concat(wordCountText, 0);
+  characterCount.textContent = concat(characterCountText, 0);
+  numberCount.textContent = concat(numberCountText, 0);
+  characterNoSpacesCount.textContent = concat(characterNoSpacesCountText, 0);
+  numberSum.textContent = concat(numberSumText, 0);
+  wordLengthAverage.textContent = concat(wordLengthAverageText, 0);
+
+}
+
